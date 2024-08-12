0cd26d9cc90d9456ee1f0000a3a62ee7a814909a
w311ang <30381083+w311ang@users.noreply.github.com>
2024-08-13 01:02:55 +0800

    Update singboxconverter_flask.py

--- singboxconverter_flask.py
+++ singboxconverter_flask.py
@@ -15,11 +15,8 @@ def root():
 	subs = params_dict.pop('sub')
 	templateurl = params_dict.pop('config', ['https://w311ang.github.io/my_singbox_template/index.yml'])[0]
 	debug = True if params_dict.pop('debug', ['false'])[0] == 'true' else False
-	for param_key, param_value in params_dict.items():
+	for param_key, param_value in {k: v for k, v in params_dict.items() if v}.items():
 		param_value=param_value[0]
-		if not param_value:
-			del params_dict[param_key]
-			continue
 		try:
 			params_dict[param_key]=json.loads(param_value)
 		except json.decoder.JSONDecodeError:
