7bc7df5ef5ac7620db2d06986388b2adde21f1d1
w311ang <30381083+w311ang@users.noreply.github.com>
2024-08-12 16:51:42 +0800

    Update singboxconverter_flask.py

--- singboxconverter_flask.py
+++ singboxconverter_flask.py
@@ -13,13 +13,14 @@ atexit.register(lambda: converter.close())
 def root():
 	params_dict = request.args.to_dict(flat=False)
 	subs = params_dict.pop('sub')
-	templateurl = params_dict.pop('config', 'https://w311ang.github.io/my_singbox_template/index.yml')
-	debug = True if params_dict.pop('debug', 'false') == 'true' else False
+	templateurl = params_dict.pop('config', ['https://w311ang.github.io/my_singbox_template/index.yml'])[0]
+	debug = True if params_dict.pop('debug', ['false'])[0] == 'true' else False
 	for param_key, param_value in params_dict.items():
+		param_value=param_value[0]
 		try:
 			params_dict[param_key]=json.loads(param_value)
 		except json.decoder.JSONDecodeError:
-			params_dict[param_key]=param_value
+			params_dict[param_key]=param_value.split(',') if ',' in param_value else param_value
 
 	subs=[sub.split(',') for sub in subs]
 	subconfig=[
