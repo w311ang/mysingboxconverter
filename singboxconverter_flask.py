146270db5b83fe49e2d4b7823a33d8ca841f215c
w311ang <30381083+w311ang@users.noreply.github.com>
2024-08-12 16:15:30 +0800

    Update singboxconverter_flask.py

--- singboxconverter_flask.py
+++ singboxconverter_flask.py
@@ -13,20 +13,20 @@ atexit.register(lambda: converter.close())
 def root():
 	params_dict = request.args.to_dict(flat=False)
 	subs = params_dict.pop('sub')
-	configurl = params_dict.pop('config', 'https://w311ang.github.io/my_singbox_template/index.yml')
+	templateurl = params_dict.pop('config', 'https://w311ang.github.io/my_singbox_template/index.yml')
 	for param_key, param_value in params_dict.items():
 		try:
 			params_dict[param_key]=json.loads(param_value)
 		except json.decoder.JSONDecodeError:
 			params_dict[param_key]=param_value
 
-	subs=[sub.split(',') in subs]
+	subs=[sub.split(',') for sub in subs]
 	subconfig=[
 		{
-			'suburl': params[0],
-			'is_sing_box_format': bool(int(params[1])) if len(params) >= 2 else False,
-			'include_all_outbounds': bool(int(params[2])) if len(params) >= 3 else False
-		} for config in subs
+			'suburl': sub[0],
+			'is_sing_box_format': bool(int(sub[1])) if len(sub) >= 2 else False,
+			'include_all_outbounds': bool(int(sub[2])) if len(sub) >= 3 else False
+		} for sub in subs
 	]
 
 	if request.args.get('debug', 'false') == 'true':
@@ -35,5 +35,5 @@ def root():
 		debug = False
 
 	with httpx.Client() as client:
-		config=client.get(configurl).text
-	return converter.convert(subconfig, config, subs_params=subs_params, debug=debug)
+		template=client.get(templateurl).text
+	return converter.convert(subconfig, template, subs_params=subs_params, debug=debug)
